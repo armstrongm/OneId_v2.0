99608fb99987d881b8f0830d47da1e1d4643f891
armstrongm <cuteronin@yahoo.co.uk>
2025-09-07 15:43:25 -0500

    new version uploaded on 07SEP2025
    new version uploaded on 07SEP2025

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+        has: [
+          {
+            type: 'cookie',
+            key: 'access_token',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
